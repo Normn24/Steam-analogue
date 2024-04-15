7edddf1b8b1efd49c23da5e9b5c92f908dde4778
ReDKaen <rigiy.yojik@gmail.com>
2024-04-15 17:19:09 +0300

    save

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import { NavBarWrapper, NavBarItem, StyledNavBarLink } from "./NacBarStyled"
+export default function NavBar() {
+    return (
+        <NavBarWrapper>
+            <NavBarItem>
+                <StyledNavBarLink>Топ рейтингу</StyledNavBarLink>
+            </NavBarItem>
+            <NavBarItem>
+                <StyledNavBarLink>Топ рейтингу</StyledNavBarLink>
+            </NavBarItem>
+            <NavBarItem>
+                <StyledNavBarLink>Топ рейтингу</StyledNavBarLink>
+            </NavBarItem>
+            <NavBarItem>
+                <StyledNavBarLink>Топ рейтингу</StyledNavBarLink>
+            </NavBarItem>
+        </NavBarWrapper>
+    )
+}
