70a736b24efadf2250d49b9e31de682c27f9ee59
ReDKaen <rigiy.yojik@gmail.com>
2024-04-20 12:18:52 +0300

    fix footer and navbar

--- src/components/Footer/FooterStyle.js
+++ src/components/Footer/FooterStyle.js
@@ -1,31 +1,33 @@
-import styled from '@emotion/styled';
-import { NavLink } from "react-router-dom";
+import { styled } from '@mui/system';
+import { Typography } from '@mui/material';
+import { NavLink } from 'react-router-dom';
 
-export const StyledFooter = styled.footer`
-    width: 100%;
-    height: 232px;
-    background-color: #F0F0F4;
-    display: flex;
-    justify-content: center;
-    gap: 40px;
-`;
+export const StyledFooter = styled('footer')({
+    width: '100%',
+    height: '232px',
+    backgroundColor: '#F0F0F4',
+    display: 'flex',
+    justifyContent: 'center',
+    gap: '40px',
+});
 
-export const InfoWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-`
+export const InfoWrapper = styled('div')({
+    display: 'flex',
+    flexDirection: 'column',
+});
 
-export const StyledTitle = styled.p`
-    color: #2f3035;
-    font-family: "Gill Sans", sans-serif;
-    font-size: 24px;
-    font-weight: 700;
-`
+export const StyledTitle = styled(Typography)({
+    color: '#2f3035',
+    fontFamily: '"Gill Sans", sans-serif',
+    fontSize: '24px',
+    fontWeight: 700,
+});
 
-export const StyledNavLink = styled(NavLink)`
-    color: #2f3035;
-    text-decoration: none;
-    font-family: "Gill Sans", sans-serif;
-    font-size: 20px;
-    font-weight: 500;
-`
+export const StyledNavLink = styled(NavLink)({
+    color: '#2f3035',
+    textDecoration: 'none',
+    fontFamily: '"Gill Sans", sans-serif',
+    fontSize: '20px',
+    fontWeight: 500,
+});
+
