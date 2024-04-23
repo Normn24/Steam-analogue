cac2ba3ce9bece39c78d8990b59ffb5f4c9ee4be
8olifirenko <8olifirenko@gmail.com>
2024-04-23 21:16:35 +0300

    fix slider

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers';
+import productsSlice from './products.slice/products.slice';
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    products: productsSlice,
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
