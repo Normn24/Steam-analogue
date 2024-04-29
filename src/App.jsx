525013c566f6e5f03742d37ccd29f361c1b75890
Anton Antimijchuk <anton.antimijchuk@gmail.com>
2024-04-29 22:19:44 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,21 @@ import Prodcts from "./components/Products/Products";
 import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
 import "./App.css";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from "./pages/home/Home";
 import ProductPage from "./pages/productPage/productPage";
 
-const router = createBrowserRouter([
-  {
-    //path: "products/:id",
-    path: "/",
-    element: <Home />,
-    //errorElement: <ErrorFallback />,
-  },
-  {
-    //path: "products/:id",
-    path: "products",
-    element: <ProductPage />,
-    //errorElement: <ErrorFallback />,
-  }
-]);
+
 function App() {
   return (
     <>
-      {/* <RouterProvider router={router} /> */}
       {<NavBar />}
-      {<ProductPage />}
-      <Prodcts />
+      <Routes>
+        
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+        
+      </Routes>
       {<Footer />}
     </>
   );
--- src/pages/productPage/productPage.jsx
+++ src/pages/productPage/productPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import { fetchProductId } from "../../redux/productItem.slice/productItem.slice";
 import styles from './styles.module.scss'
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
@@ -8,44 +9,45 @@ import { Rating } from '@mui/material';
 import { TabContext, TabPanel } from '@mui/lab';
 
 
-const ProductPage = ({ productId }) => {
-  const [product, setProduct] = useState({name: 'DARK SOULS REMASTERED', year: '24-05-2018', developer: 'QLOC', publisher: 'Bandai Namco Entertainment , From Softwate inc', rating: 84, genres: 'Соулс-лайк, Темне фентезі, Рольова гра, RPG', imageUrl: 'https://cdn.cloudflare.steamstatic.com/steam/apps/570940/header.jpg?t=1700659167', price: 799, description: "Потім була пожежа. Знову відчуйте визнану критиками гру, що визначає жанр, з якої все почалося. Прекрасно оновлений, поверніться до Lordran у приголомшливій високій чіткості деталей зі швидкістю 60 кадрів в секунду. Dark Souls Remastered включає основну гру та DLC Artorias of the Abyss. Ключові особливості: • Глибокий і Темний Всесвіт • Кожен кінець — це новий початок • Багатство та можливості ігрового процесу • Почуття навчання, майстерності та досягнення • Шлях багатокористувацької гри (до 6 гравців із виділеними серверами)"});
+const ProductPage = () => {
+  //const [product, setProduct] = useState({name: 'DARK SOULS REMASTERED', year: '24-05-2018', developer: 'QLOC', publisher: 'Bandai Namco Entertainment , From Softwate inc', rating: 84, genres: 'Соулс-лайк, Темне фентезі, Рольова гра, RPG', imageUrl: 'https://cdn.cloudflare.steamstatic.com/steam/apps/570940/header.jpg?t=1700659167', price: 799, description: "Потім була пожежа. Знову відчуйте визнану критиками гру, що визначає жанр, з якої все почалося. Прекрасно оновлений, поверніться до Lordran у приголомшливій високій чіткості деталей зі швидкістю 60 кадрів в секунду. Dark Souls Remastered включає основну гру та DLC Artorias of the Abyss. Ключові особливості: • Глибокий і Темний Всесвіт • Кожен кінець — це новий початок • Багатство та можливості ігрового процесу • Почуття навчання, майстерності та досягнення • Шлях багатокористувацької гри (до 6 гравців із виділеними серверами)"});
   //setProduct({name: 'DARK SOULS REMASTERED', year: '24-05-2018', developer: 'QLOC', publisher: 'Bandai Namco Entertainment , From Softwate inc', rating: 84, genres: 'Соулс-лайк, Темне фентезі, Рольова гра, RPG', imageUrl: 'https://cdn.cloudflare.steamstatic.com/steam/apps/570940/header.jpg?t=1700659167'})
-  
-  //const dispatch = useDispatch();
-  //const { product, status } = useSelector((state) => state.product);
-  
-  //sx={{marginTop: '10px', borderCollapse: 'collapse', border-spacing: '0'}}
+  const {id} = useParams();
+  const dispatch = useDispatch();
+  const { product, status } = useSelector((state) => state.product);
   
   const [favorite, setFavorite] = useState(false);
+  const handleFavorite = () => setFavorite(!favorite);
   const [value, setValue] = useState('1');
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   console.log(product);
+  console.log(status == 'loading');
 
-  /*useEffect(() => {
-    dispatch(fetchProductId());
-  }, [dispatch]);*/
+  useEffect(() => {
+    dispatch(fetchProductId(id));
+  }, [dispatch]);
 
-  if (!product) {
+  if (status == 'loading') {
     return <div className={styles.productContainer}>Loading...</div>;
   }
 
   return (
-    <Container sx={{width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', margin: '20px 0', gap: '20px', fontFamily: '"Gill Sans", sans-serif'}}>
+    <>
+    {status == 'loading' ? <><Box component="div" className={styles.productContainer}>Loading...</Box></> : <><Container sx={{width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', margin: '20px 0', gap: '20px', fontFamily: '"Gill Sans", sans-serif'}}>
       <Box component="div" sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start', gap: '50px'}}>
-        <Box component="div" sx={{marginTop: '25px', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', position: 'relative', width: '60%'}}>
-          <Button variant='contained' sx={{position: 'absolute', width: '30%'}}>Sale</Button>
-          <img src={product.imageUrl} alt={product.name} className={styles.productImg}/>
-          <h2>{product.price} ₴</h2> 
+        <Box component="div" sx={{marginTop: '25px', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', textAlign: 'center', position: 'relative', width: '60%'}}>
+          <Button variant='contained' disableElevation sx={{position: 'absolute', width: '30%', borderRadius: '0'}}>Sale</Button>
+          <img src={product?.imageUrls} alt={product.name} className={styles.productImg}/>
+          <h2>{product.currentPrice} ₴</h2> 
         </Box>
         <Box component="div" sx={{textAlign: 'left'}}>
            <h3>{product.name}</h3>
-           <Box component="div" sx={{display: 'flex', justifyContent: 'flex-start', gap: '20p'}}>
-             {!favorite && <AiOutlineHeart style={{height: '25px', width: 'auto', color: '#828386'}}/>}
-             {favorite && <AiFillHeart style={{height: '25px', width: 'auto', color: '#828386'}}/>}
+           <Box component="div" sx={{display: 'flex', justifyContent: 'flex-start', gap: '20px'}}>
+             {!favorite && <AiOutlineHeart onClick={handleFavorite} style={{height: '25px', width: 'auto', color: '#828386'}}/>}
+             {favorite && <AiFillHeart onClick={handleFavorite} style={{height: '25px', width: 'auto', color: '#828386'}}/>}
              <Box component="div" sx={{display: 'flex', alignItems: 'center', gap: '12px'}}><Rating name="half-rating" defaultValue={product.rating / 100 * 5} precision={0.1} /><span>{product.rating} / 100</span></Box>
            </Box>
            
@@ -100,7 +102,9 @@ const ProductPage = ({ productId }) => {
       </TabContext>
       </Box>
       <div className="products-carousel"></div>
-    </Container>
+    </Container></>}
+    </>
+    
   );
 };
 
