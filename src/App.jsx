8b0fee71523a6eb570255f73b9678de1cdf92423
Den <den204d@gmail.com>
2024-05-01 21:11:20 +0300

    fix photo in product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import Prodcts from "./components/Products/Products";
+// import Prodcts from "./components/Products/Products";
 import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
 import "./App.css";
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import ProductPage from "./pages/productPage/productPage";
-
 
 function App() {
   return (
     <>
-      {<NavBar />}
+      <NavBar />
       <Routes>
-        
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:id" element={<ProductPage />} />
-        
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductPage />} />
       </Routes>
       {<Footer />}
     </>
   );
 }
 
-export default App
+export default App;
--- src/pages/productPage/productPage.jsx
+++ src/pages/productPage/productPage.jsx
@@ -1,60 +1,144 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchProductId } from "../../redux/productItem.slice/productItem.slice";
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
-import { Container, Box, Tabs, Tab, Table, TableBody, TableRow, TableCell, Button } from '@mui/material';
-import { Rating } from '@mui/material';
-import { TabContext, TabPanel } from '@mui/lab';
-
+import {
+  Container,
+  Box,
+  Tabs,
+  Tab,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+  Button,
+} from "@mui/material";
+import { Rating } from "@mui/material";
+import { TabContext, TabPanel } from "@mui/lab";
 
 const ProductPage = () => {
-  //const [product, setProduct] = useState({name: 'DARK SOULS REMASTERED', year: '24-05-2018', developer: 'QLOC', publisher: 'Bandai Namco Entertainment , From Softwate inc', rating: 84, genres: 'Соулс-лайк, Темне фентезі, Рольова гра, RPG', imageUrl: 'https://cdn.cloudflare.steamstatic.com/steam/apps/570940/header.jpg?t=1700659167', price: 799, description: "Потім була пожежа. Знову відчуйте визнану критиками гру, що визначає жанр, з якої все почалося. Прекрасно оновлений, поверніться до Lordran у приголомшливій високій чіткості деталей зі швидкістю 60 кадрів в секунду. Dark Souls Remastered включає основну гру та DLC Artorias of the Abyss. Ключові особливості: • Глибокий і Темний Всесвіт • Кожен кінець — це новий початок • Багатство та можливості ігрового процесу • Почуття навчання, майстерності та досягнення • Шлях багатокористувацької гри (до 6 гравців із виділеними серверами)"});
-  //setProduct({name: 'DARK SOULS REMASTERED', year: '24-05-2018', developer: 'QLOC', publisher: 'Bandai Namco Entertainment , From Softwate inc', rating: 84, genres: 'Соулс-лайк, Темне фентезі, Рольова гра, RPG', imageUrl: 'https://cdn.cloudflare.steamstatic.com/steam/apps/570940/header.jpg?t=1700659167'})
-  const {id} = useParams();
+  const { id } = useParams();
+  console.log(id);
   const dispatch = useDispatch();
   const { product, status } = useSelector((state) => state.product);
-  
+
   const [favorite, setFavorite] = useState(false);
   const handleFavorite = () => setFavorite(!favorite);
-  const [value, setValue] = useState('1');
+  const [value, setValue] = useState("1");
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   console.log(product);
-  console.log(status == 'loading');
+  console.log(status == "loading");
 
   useEffect(() => {
     dispatch(fetchProductId(id));
-  }, [dispatch]);
+  }, [dispatch, id]);
 
-  if (status == 'loading') {
+  if (status == "loading") {
     return <div className={styles.productContainer}>Loading...</div>;
   }
 
   return (
-    <>
-    {status == 'loading' ? <><Box component="div" className={styles.productContainer}>Loading...</Box></> : <><Container sx={{width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', margin: '20px 0', gap: '20px', fontFamily: '"Gill Sans", sans-serif'}}>
-      <Box component="div" sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start', gap: '50px'}}>
-        <Box component="div" sx={{marginTop: '25px', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', textAlign: 'center', position: 'relative', width: '60%'}}>
-          <Button variant='contained' disableElevation sx={{position: 'absolute', width: '30%', borderRadius: '0'}}>Sale</Button>
-          <img src={product?.imageUrls} alt={product.name} className={styles.productImg}/>
-          <h2>{product.currentPrice} ₴</h2> 
+    <Container
+      sx={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        margin: "20px 0",
+        gap: "20px",
+        fontFamily: '"Gill Sans", sans-serif',
+      }}
+    >
+      <Box
+        component="div"
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "flex-start",
+          gap: "50px",
+        }}
+      >
+        <Box
+          component="div"
+          sx={{
+            marginTop: "25px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            textAlign: "center",
+            position: "relative",
+            width: "60%",
+          }}
+        >
+          <Button
+            variant="contained"
+            disableElevation
+            sx={{ position: "absolute", width: "30%", borderRadius: "0" }}
+          >
+            Sale
+          </Button>
+
+          <img
+            src={product.imageUrls ? product.imageUrls[1] : ""}
+            alt={product.name}
+            className={styles.productImg}
+          />
+
+          <h2>{product.currentPrice} ₴</h2>
         </Box>
-        <Box component="div" sx={{textAlign: 'left'}}>
-           <h3>{product.name}</h3>
-           <Box component="div" sx={{display: 'flex', justifyContent: 'flex-start', gap: '20px'}}>
-             {!favorite && <AiOutlineHeart onClick={handleFavorite} style={{height: '25px', width: 'auto', color: '#828386'}}/>}
-             {favorite && <AiFillHeart onClick={handleFavorite} style={{height: '25px', width: 'auto', color: '#828386'}}/>}
-             <Box component="div" sx={{display: 'flex', alignItems: 'center', gap: '12px'}}><Rating name="half-rating" defaultValue={product.rating / 100 * 5} precision={0.1} /><span>{product.rating} / 100</span></Box>
-           </Box>
-           
-           <Table >
-              <TableBody>
+        <Box component="div" sx={{ textAlign: "left" }}>
+          <h3>{product.name}</h3>
+          <Box
+            component="div"
+            sx={{
+              display: "flex",
+              justifyContent: "flex-start",
+              gap: "20px",
+            }}
+          >
+            {!favorite && (
+              <AiOutlineHeart
+                onClick={handleFavorite}
+                style={{
+                  height: "25px",
+                  width: "auto",
+                  color: "#828386",
+                }}
+              />
+            )}
+            {favorite && (
+              <AiFillHeart
+                onClick={handleFavorite}
+                style={{
+                  height: "25px",
+                  width: "auto",
+                  color: "#828386",
+                }}
+              />
+            )}
+            <Box
+              component="div"
+              sx={{ display: "flex", alignItems: "center", gap: "12px" }}
+            >
+              <Rating
+                name="half-rating"
+                defaultValue={(product.rating / 100) * 5}
+                precision={0.1}
+              />
+              <span>{product.rating} / 100</span>
+            </Box>
+          </Box>
+
+          <Table>
+            <TableBody>
               <TableRow>
-                <TableCell sx={{ minWidth: '150px' }}>Дата виходу</TableCell>
+                <TableCell sx={{ minWidth: "150px" }}>Дата виходу</TableCell>
                 <TableCell>{product.year}</TableCell>
               </TableRow>
               <TableRow>
@@ -69,42 +153,65 @@ const ProductPage = () => {
                 <TableCell>Жанр</TableCell>
                 <TableCell>{product.genres}</TableCell>
               </TableRow>
-              </TableBody>
-           </Table>
-           <Button variant='contained' sx={{marginTop: '20px', fontSize: '18px', width: '30%'}}>Купити</Button>
+            </TableBody>
+          </Table>
+          <Button
+            variant="contained"
+            sx={{ marginTop: "20px", fontSize: "18px", width: "30%" }}
+          >
+            Купити
+          </Button>
         </Box>
       </Box>
       <Box component="div">
-      <TabContext value={value}>
-       <Box sx={{borderBottom: 1, borderColor: 'divider', backgroundColor: '#fff'}}>
-        <Tabs
-        value={value}
-        onChange={handleChange}
-        centered
-        variant="fullWidth"
-        
-        TabIndicatorProps={{sx: {backgroundColor: 'blue', color: 'black'}}}
-        aria-label="secondary tabs example"
-        sx ={{
-          border: 'none',
-          "& button": {},
-          "& button:hover": {backgroundColor: '#c4c4c4', fontWeight: 500},
-          "& button:active": {borderColor: 'transparent'},
-          "& button:Mui-selected": {backgroundColor: 'transparent'}
-        }}
-        >
-          <Tab value="1" label="Опис" />
-          <Tab value="2" label="Системні вимоги" />
-        </Tabs>
-       </Box>
-        <TabPanel value="1" index={0} sx={{textAlign: 'left', fontSize: '18px'}}>{product.description}</TabPanel>
-        <TabPanel value="2" index={1}>Tab Two</TabPanel>
-      </TabContext>
+        <TabContext value={value}>
+          <Box
+            sx={{
+              borderBottom: 1,
+              borderColor: "divider",
+              backgroundColor: "#fff",
+            }}
+          >
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              centered
+              variant="fullWidth"
+              TabIndicatorProps={{
+                sx: { backgroundColor: "blue", color: "black" },
+              }}
+              aria-label="secondary tabs example"
+              sx={{
+                border: "none",
+                "& button": {},
+                "& button:hover": {
+                  backgroundColor: "#c4c4c4",
+                  fontWeight: 500,
+                },
+                "& button:active": { borderColor: "transparent" },
+                "& button:Mui-selected": {
+                  backgroundColor: "transparent",
+                },
+              }}
+            >
+              <Tab value="1" label="Опис" />
+              <Tab value="2" label="Системні вимоги" />
+            </Tabs>
+          </Box>
+          <TabPanel
+            value="1"
+            index={0}
+            sx={{ textAlign: "left", fontSize: "18px" }}
+          >
+            {product.description}
+          </TabPanel>
+          <TabPanel value="2" index={1}>
+            Tab Two
+          </TabPanel>
+        </TabContext>
       </Box>
       <div className="products-carousel"></div>
-    </Container></>}
-    </>
-    
+    </Container>
   );
 };
 
