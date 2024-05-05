82ed7b4f989d03a748b9a7a37fc4fba06494933e
Den <den204d@gmail.com>
2024-05-05 19:26:36 +0300

    hotfix

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,75 +1,74 @@
 import { NavBarItem, NavBarWrapper, StyledNavBarLink } from "./NavBarStyled";
-import { fetchLinks } from "../../redux/links.slice/links.slice";
 import { fetchCatalogs } from "../../redux/catalogs.slice/catalogs.slice";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-import TextField from '@mui/material/TextField';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function NavBar() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const { links } = useSelector((state) => state.links);
-    const { catalogs, status, error } = useSelector((state) => state.catalogs);
-    
-    const [filterLinks, setFilterLinks] = useState([]);
-    const [linkToNav, setLinkToNav] = useState('');
-    const [searchQuery, setSearchQuery] = useState(''); 
+  const { catalogs, status, error } = useSelector((state) => state.catalogs);
 
-    const handleSearchChange = (event) => {
-        setSearchQuery(event.target.value);
-    };
+  //   const [filterLinks, setFilterLinks] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
-    const handleSearchSubmit = (event) => {
-        event.preventDefault();
-        onSearch(searchQuery);
-    };
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
 
-    useEffect(() => {
-        dispatch(fetchLinks());
-        dispatch(fetchCatalogs());
-    }, [dispatch]);
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    // onSearch(searchQuery);
+  };
 
-    useEffect(() => {
-        if (Array.isArray(links)) {
-            const filtered = links.filter(link => link.title && link.title.includes("NAVBAR"));
-            setFilterLinks(filtered);
-        }
-    }, [links]);
+  useEffect(() => {
+    dispatch(fetchCatalogs());
+  }, [dispatch]);
 
-    useEffect(() => {
-        if (filterLinks.length > 0) {
-            setLinkToNav(filterLinks[0]?.links[0]?.url || '');
-        }
-    }, [filterLinks]);
+  //   useEffect(() => {
+  //     if (Array.isArray(links)) {
+  //       const filtered = links.filter(
+  //         (link) => link.title && link.title.includes("NAVBAR")
+  //       );
+  //       setFilterLinks(filtered);
+  //     }
+  //   }, [links]);
 
-    return (
-        <NavBarWrapper>
-            {status === 'loading' && <p>Loading...</p>}
-            {status === 'failed' && <p>Error: {error}</p>}
-            {status === 'succeeded' && Array.isArray(catalogs) && (
-                catalogs.map((catalog) => (
-                    <NavBarItem key={catalog.id}>
-                        <StyledNavBarLink to={linkToNav}>{catalog.name}</StyledNavBarLink>
-                    </NavBarItem>
-                ))
-            )}
-            <form onSubmit={handleSearchSubmit} style={{ margin: "0 auto" }}>
-                <TextField
-                    placeholder="Search..."
-                    variant="outlined"
-                    value={searchQuery}
-                    onChange={handleSearchChange}
-                    InputProps={{
-                        endAdornment: (
-                            <IconButton type="submit" aria-label="search">
-                                <SearchIcon />
-                            </IconButton>
-                        ),
-                    }}
-                />
-            </form>
-        </NavBarWrapper>
-    );
+  //   useEffect(() => {
+  //     if (filterLinks.length > 0) {
+  //       setLinkToNav(filterLinks[0]?.links[0]?.url || "");
+  //     }
+  //   }, [filterLinks]);
+
+  return (
+    <NavBarWrapper>
+      {status === "loading" && <p>Loading...</p>}
+      {status === "failed" && <p>Error: {error}</p>}
+      {status === "succeeded" &&
+        catalogs.slice(0, 4).map((catalog) => (
+          <NavBarItem key={catalog.id}>
+            <StyledNavBarLink to={`/products/category=${catalog.name}`}>
+              {catalog.name}
+            </StyledNavBarLink>
+          </NavBarItem>
+        ))}
+      <form onSubmit={handleSearchSubmit} style={{ margin: "0 auto" }}>
+        <TextField
+          placeholder="Search..."
+          variant="outlined"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          InputProps={{
+            endAdornment: (
+              <IconButton type="submit" aria-label="search">
+                <SearchIcon />
+              </IconButton>
+            ),
+          }}
+        />
+      </form>
+    </NavBarWrapper>
+  );
 }
