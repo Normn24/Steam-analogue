55aadb058347e3013ece96955d47c9c58d605035
zhadanovdaniel <zhadanovdaniel21@gmail.com>
2024-05-14 00:18:24 +0200

    resolved conflicts

--- src/components/Button/Button.jsx
+++
@@ -1,4 +0,0 @@
-export default function Button(props) {
-  const { type, classNames, onClick, children } = props;
-  return <button className={classNames} onClick={onClick}> {children} </button>;
-}
