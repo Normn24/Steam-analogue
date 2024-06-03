5a59f50d2b440ae5ec3d21a52a99f905c4f79e9e
Den <den204d@gmail.com>
2024-06-03 14:42:00 +0300

    upd website title and icon

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import {
   persistReducer, persistStore,
-  // FLUSH,
-  // REHYDRATE,
-  // PAUSE,
-  // PERSIST,
-  // PURGE,
-  // REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import productItemSlice from './productItem.slice/productItem.slice';
 import productsSlice from './products.slice/products.slice';
 import slidesSlice from './slides.slice/slides.slice';
-import linksSlice from './links.slice/links.slice';
 import catalogsSlice from './catalogs.slice/catalogs.slice';
 import genresSlice from './genres.slice/genres.slice';
 import catalogProductsSlice from './catalogProducts.slice/catalogProducts.slice';
@@ -42,7 +35,6 @@ export const store = configureStore({
     product: productItemSlice,
     products: productsSlice,
     slides: slidesSlice,
-    links: linksSlice,
     catalogs: catalogsSlice,
     genres: genresSlice,
     categoriesProducts: catalogProductsSlice,
